87b6686a54cbc321d89cd20f618bf8f31c0a729e
Sambhav Sachdeva <ssachdeva25@myseneca.ca>
2024-05-23 16:00:46 -0400

    lab2c.py

---
+++ lab2c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Assign the command-line arguments to variables
+name = sys.argv[1]
+age = int(sys.argv[2])
+
+# Print the required output
+print(f"Hi {name}, you are {age} years old.")
+
+
